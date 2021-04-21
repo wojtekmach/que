52a05bf7913d74a10bd52e0c6a8a75febe717cb1
Wojtek Mach <wojtek@wojtekmach.pl>
2021-04-21 20:20:01 +0200

    Add Que.FS

--- lib/que.ex
+++ lib/que.ex
@@ -52,3 +52,35 @@ defmodule Que.ERTS do
     Enum.take(stream, limit)
   end
 end
+
+defmodule Que.FS do
+  def run(%{from: dir} = query) do
+    _ = query
+
+    Path.wildcard("#{dir}/**")
+    |> Stream.map(fn path ->
+      stat = File.stat!(path)
+      path = Path.relative_to(path, dir)
+      Map.merge(%{path: path}, Map.from_struct(stat))
+    end)
+    |> order_by(query[:order_by])
+    |> Enum.map(&select(&1, query[:select] || :*))
+  end
+
+  defp select(item, :*) do
+    item
+  end
+
+  defp select(item, fields) when is_list(fields) do
+    Map.new(fields, &{&1, Map.get(item, &1)})
+  end
+
+  defp order_by(stream, nil) do
+    Enum.to_list(stream)
+  end
+
+  defp order_by(stream, field) do
+    Enum.to_list(stream)
+    |> Enum.sort_by(&Map.fetch!(&1, field))
+  end
+end
--- test/que_test.exs
+++ test/que_test.exs
@@ -2,22 +2,47 @@ defmodule QueTest do
   use ExUnit.Case, async: true
   import Que
 
-  test "it works" do
-    q(
-      select: [:pid, :registered_name],
-      from: :processes,
-      where: :heap_size < 1000,
-      limit: 3
-    )
-    |> Que.ERTS.run()
-    |> IO.inspect()
+  test "ERTS" do
+    q =
+      q(
+        select: [:pid, :registered_name],
+        from: :processes,
+        where: :heap_size < 1000,
+        limit: 3
+      )
 
-    q(
-      select: :*,
-      from: :processes,
-      where: :registered_name == :logger
-    )
-    |> Que.ERTS.run()
-    |> IO.inspect()
+    procs = Que.ERTS.run(q)
+    assert length(procs) == 3
+
+    q =
+      q(
+        select: :*,
+        from: :processes,
+        where: :registered_name == :logger
+      )
+
+    [proc] = Que.ERTS.run(q)
+    assert proc.pid == Process.whereis(:logger)
+  end
+
+  test "FS" do
+    File.rm_rf!("tmp")
+    File.mkdir_p!("tmp")
+    File.write!("tmp/b.txt", "")
+    sleep_until_next_second()
+    File.write!("tmp/a.txt", "")
+
+    q = q(select: [:path], from: "tmp", order_by: :path)
+    assert Que.FS.run(q) == [%{path: "a.txt"}, %{path: "b.txt"}]
+
+    q = q(select: [:path], from: "tmp", order_by: :mtime)
+    assert Que.FS.run(q) == [%{path: "b.txt"}, %{path: "a.txt"}]
+  end
+
+  defp sleep_until_next_second() do
+    time = System.system_time(:millisecond)
+    seconds = div(time, 1000)
+    delta = (seconds + 1) * 1000 - time
+    Process.sleep(delta)
   end
 end
