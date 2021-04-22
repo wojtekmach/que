351f3ba0c8cb3be448b471b22a92309cc466ddc8
Wojtek Mach <wojtek@wojtekmach.pl>
2021-04-22 06:58:01 +0200

    DRY up

--- lib/que.ex
+++ lib/que.ex
@@ -13,12 +13,12 @@ defmodule Que do
   end
 end
 
-defmodule Que.ERTS do
-  def run(%{from: :processes} = query) do
-    Process.list()
-    |> Stream.map(&(&1 |> Process.info() |> Map.new() |> Map.put(:pid, &1)))
+defmodule Que.Stream do
+  def run(%{from: stream} = query) do
+    stream
     |> Stream.filter(&filter(&1, query[:where]))
-    |> Stream.map(&select(&1, query[:select] || :*))
+    |> order_by(query[:order_by])
+    |> Enum.map(&select(&1, query[:select] || :*))
     |> limit(query[:limit])
   end
 
@@ -44,6 +44,15 @@ defmodule Que.ERTS do
     v && v == value
   end
 
+  defp order_by(stream, nil) do
+    Enum.to_list(stream)
+  end
+
+  defp order_by(stream, field) do
+    Enum.to_list(stream)
+    |> Enum.sort_by(&Map.fetch!(&1, field))
+  end
+
   defp limit(stream, nil) do
     Enum.to_list(stream)
   end
@@ -53,34 +62,30 @@ defmodule Que.ERTS do
   end
 end
 
+defmodule Que.ERTS do
+  def run(%{from: :processes} = query) do
+    stream = Stream.map(Process.list(), &(&1 |> Process.info() |> Map.new() |> Map.put(:pid, &1)))
+
+    %{query | from: stream}
+    |> Que.Stream.run()
+    |> Enum.to_list()
+  end
+end
+
 defmodule Que.FS do
   def run(%{from: dir} = query) do
     _ = query
 
-    Path.wildcard("#{dir}/**")
-    |> Stream.map(fn path ->
-      stat = File.stat!(path)
-      path = Path.relative_to(path, dir)
-      Map.merge(%{path: path}, Map.from_struct(stat))
-    end)
-    |> order_by(query[:order_by])
-    |> Enum.map(&select(&1, query[:select] || :*))
-  end
+    stream =
+      Path.wildcard("#{dir}/**")
+      |> Stream.map(fn path ->
+        stat = File.stat!(path)
+        path = Path.relative_to(path, dir)
+        Map.merge(%{path: path}, Map.from_struct(stat))
+      end)
 
-  defp select(item, :*) do
-    item
-  end
-
-  defp select(item, fields) when is_list(fields) do
-    Map.new(fields, &{&1, Map.get(item, &1)})
-  end
-
-  defp order_by(stream, nil) do
-    Enum.to_list(stream)
-  end
-
-  defp order_by(stream, field) do
-    Enum.to_list(stream)
-    |> Enum.sort_by(&Map.fetch!(&1, field))
+    %{query | from: stream}
+    |> Que.Stream.run()
+    |> Enum.to_list()
   end
 end
